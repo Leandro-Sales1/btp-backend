107fd37970d3d866cf48c2a2afc52c93fd5b7c30
Leandro Sales <lsl.2014@hotmail.com>
2024-11-05 14:56:17 -0300

    "Moved express.json() middleware to top of routes, swapped order of swaggerUi and userRoutes middleware"

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,14 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from "../../swaggerConfig.js";
 
 const routes = (app) => {
+  app.use(express.json())
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
   app.get("/", (req, res) => {
     res.redirect("/api-docs");
   });
 
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-  
-  app.use(express.json(), userRoutes)
+  app.use(userRoutes)
 }
 
 export default routes;
