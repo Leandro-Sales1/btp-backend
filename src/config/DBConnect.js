95d02d5091e3e0b1a7c22c3dcafd985f8cc50f63
Leandro Sales <lsl.2014@hotmail.com>
2024-11-05 12:50:47 -0300

    "Added timeout options to mongoose connection in DBConnection function"

--- src/config/DBConnect.js
+++ src/config/DBConnect.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
 
-export default async function DBConnection(){
-  mongoose.connect(process.env.DB_CONNECTION_STRING);
+export default async function DBConnection() {
+  mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  });
   return mongoose.connection;
 }
