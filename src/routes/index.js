8abab2f6cf421389cf639ffec656431615aa8fa7
Leandro Sales <lsl.2014@hotmail.com>
2024-11-05 15:22:34 -0300

    "Downgrade swagger-ui-express from 5.0.1 to 4.6.2, update package-lock.json and package.json accordingly, and modify src/routes/index.js to add custom CSS and URL for swagger-ui-express"

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,14 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from "../../swaggerConfig.js";
 
 const routes = (app) => {
+  const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
   app.use(express.json())
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+  }));
 
   app.get("/", (req, res) => {
     res.redirect("/api-docs");
